ffd576d5a7b55178389fb5fce0fd3374656d364b
Luke Salazar <lucassy07@outlook.com>
2024-02-06 16:42:10 -0500

    first commit, Spring boot project

---
+++ src/main/java/com/example/tiendapp_backend/TiendappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiendapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendappBackendApplication.class, args);
+	}
+
+}
